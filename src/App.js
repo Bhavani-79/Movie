c9df3f863dc5c5fa79339cd59f54370bc5761704
Bhavani <durgabhavanik456@gmail.com>
2025-06-25 18:16:57 +0530

    added topRated,Upcoming Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './component/Home'
+import TopRated from './component/TopRated';
+import UpComing from './component/UpComing';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path="/" Component={Home} />
+      <Route exact path="/top-rated"  Component={TopRated}/>
+      <Route exact path="/upcoming"  Component={UpComing}/>
     </Routes>
   </BrowserRouter>
 )
